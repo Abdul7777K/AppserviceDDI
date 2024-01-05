5f9e570fb3097428191c93972efa915720a29977
Abdul <Abdul@example.com>
2024-01-05 09:52:59 +0530

    app service5

--- providers.tf
+++ providers.tf
@@ -1,7 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
 
   }
+  #subscription_id = "005a3d05-8323-4d09-80d1-420458866ed6"
+  #tenant_id       = "aee1a4fd-c521-4c4d-ba71-80d7b1ea77d3"
+  #client_id       = "c42e48d2-2eb6-4bec-b3a0-406c98732d15"
+  #client_secret   = "Ssd8Q~a.RVO2du3b3xD6h4~unr8GVY6yIKA_wcif"
 }
 
 data "terraform_remote_state" "workspace_b" {
@@ -12,10 +25,4 @@ data "terraform_remote_state" "workspace_b" {
       name = "Vnetddi"
     }
   }
-}
-
-data "azuread_client_config" "current" {}
-
-output "object_id" {
-  value = data.azuread_client_config.current.object_id
 }

6f7c53f33d507ccd60264f1eaaec53acaaa409ee
Abdul <Abdul@example.com>
2024-01-05 12:09:13 +0530

    app service9

--- providers.tf
+++ providers.tf
@@ -11,10 +11,11 @@ provider "azurerm" {
   features {
 
   }
-  #subscription_id = "005a3d05-8323-4d09-80d1-420458866ed6"
+  ##subscription_id = "005a3d05-8323-4d09-80d1-420458866ed6"
   #tenant_id       = "aee1a4fd-c521-4c4d-ba71-80d7b1ea77d3"
   #client_id       = "c42e48d2-2eb6-4bec-b3a0-406c98732d15"
   #client_secret   = "Ssd8Q~a.RVO2du3b3xD6h4~unr8GVY6yIKA_wcif"
+  #..
 }
 
 data "terraform_remote_state" "workspace_b" {

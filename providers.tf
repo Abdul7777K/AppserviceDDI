cfb13556efca6ce95c0961ff18d63a17eba48a75
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2024-01-24 12:52:26 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,12 +18,3 @@ provider "azurerm" {
   #..
 }
 
-data "terraform_remote_state" "workspace_b" {
-  backend = "remote"
-  config = {
-    organization = "Demo-DDI"
-    workspaces = {
-      name = "Vnetddi"
-    }
-  }
-}
